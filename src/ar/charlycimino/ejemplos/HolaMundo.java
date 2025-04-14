a4fdf3bc9e737812c580481cf1b600eadaa73965
Yuki-tor <ytorres@arkusnexus.com>
2025-04-14 08:12:08 -0700

    Add files via upload

---
+++ src/ar/charlycimino/ejemplos/HolaMundo.java
@@ -0,0 +1,16 @@
+
+package ar.charlycimino.ejemplos;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+
+}
